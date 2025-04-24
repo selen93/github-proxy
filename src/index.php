bfac6a909a47e2f8dfc6c8272620db2003014878
nbar <nbar@spreadshirt.net>
2025-04-24 11:12:40 +0200

    Add a skeleton GET request handler

--- src/index.php
+++ src/index.php
@@ -1,2 +1,15 @@
 <?php
-phpinfo();
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $response = [
+        'status' => 'success',
+        'message' => 'This will soon be a github search proxy :)',
+    ];
+    echo json_encode($response);
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Only GET requests are allowed']);
+}
